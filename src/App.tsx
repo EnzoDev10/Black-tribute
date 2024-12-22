e8e45b6af18f377bb6e76b1e7fe6c6d967506892
Enzo Cañete <enzodev4@gmail.com>
2024-12-22 15:23:00 -0300

    Removed the original intro component from App and disabled background video temporally.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
 import './App.css';
-import video from './assets/videos/background.webm';
-import poster from './assets/images/bg-poster.png';
-import logo from './assets/images/logo.png';
+/* import video from './assets/videos/background.webm';
+import poster from './assets/images/bg-poster.webp'; */
 
-import { Button } from './components/Index';
+import { MainContent, Intro } from './components/Index';
 
 function App() {
-	return (
-		<div className="container overlay">
+	/* 	const [showMain, setShowMain] = useState(false);
+	 */ return (
+		<div className="container">
 			{/* background video */}
-			<video playsInline autoPlay muted loop id="bgvid" poster={poster}>
+			{/* <video playsInline autoPlay muted loop id="bgvid" poster={poster}>
 				<source src={video} type="video/webm" />
-			</video>
-			<div className="left-side" />
-			<div className="lines-container">
-				<div className="reverse line" />
-				<div className="line" />
-			</div>
-			<main className="">
-				<img src={logo} className="logo" alt="Black Logo" />
+			</video> */}
+			<Intro />
 
-				<div className="actions-container">
-					<Button label="EASY" addSquare={true} parentMethod={() => {}} />
-					<Button label="NORMAL" addSquare={false} parentMethod={() => {}} />
-					<Button label="HARD" addSquare={true} parentMethod={() => {}} />
-					<Button label="BLACK OPS" addSquare={false} parentMethod={() => {}} />
-				</div>
-			</main>
+			{/* <MainContent /> */}
 		</div>
 	);
 }
