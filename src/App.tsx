5c5e659a568d631b22564bd13c571296dfc05cd9
Enzo Cañete <enzodev4@gmail.com>
2024-12-26 22:37:03 -0300

    Added transition between Intro and Content components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from 'react';
 import './App.css';
 /* import video from './assets/videos/background.webm';
 import poster from './assets/images/bg-poster.webp'; */
@@ -5,15 +6,31 @@ import poster from './assets/images/bg-poster.webp'; */
 import { MainContent, Intro } from './components/Index';
 
 function App() {
-	/* 	const [showMain, setShowMain] = useState(false);
-	 */ return (
+	const overlay = useRef<HTMLDivElement>(null!);
+	const [isMainVisible, setIsMainVisible] = useState(false);
+	/* Use bracket notation with useRef.  */
+	/* Adds an animation to the overlay div so it appears on btn click. */
+
+	function setVisibility() {
+		overlay['current']['style']['animation'] = 'fadeOut 5s ease-in forwards';
+
+		const makeVisible = setInterval(() => {
+			setIsMainVisible(true);
+			console.log('trabajo terminado');
+			clearInterval(makeVisible);
+		}, 3000);
+	}
+
+	return (
 		<div className="container">
+			<div className="gun-bg"></div>
+			<div className="overlay-black" ref={overlay} />
 			{/* background video */}
 			{/* <video playsInline autoPlay muted loop id="bgvid" poster={poster}>
 				<source src={video} type="video/webm" />
 			</video> */}
-
-			<MainContent />
+			{!isMainVisible && <Intro appMethod={setVisibility} />}
+			{isMainVisible && <MainContent />}
 		</div>
 	);
 }
